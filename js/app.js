da8c709ade8f4d2427c097d20178a4928bd88566
arcadebrigade <dianazelikman@gmail.com>
2015-03-04 17:06:07 -0500

    updated version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
 $(document).ready(function () {
 
-	var $text=$('.text');
+	var fizzBuzz = function(userNumber) {
 
-for (var i= 1; i<=100; i++) {
-	if (i%15 == 0) {
-		console.log ("fizzbizz");
+		for (var i= 1; i<=userNumber; i++) {
+			if (i%15 == 0) {
+				//console.log ("fizzbizz");
+				text += 'fizzBuzz ';
+			}
+			else if (i%3 == 0) {
+				//console.log ("fizz");
+				text += 'fizz ';
+			}
+			else if (i%5 == 0) {
+				//console.log ("buzz");
+				text += 'buzz ';
+			}
+			else {
+				text += i + " "
+			}
+		}
+
 	}
-	else if (i%3 == 0) {
-		console.log ("fizz");
+
+	var text = '';
+
+	var userNumber = prompt('please enter a number');
+
+	//var userNumber = parseInt(promptNumber);
+
+	console.log(userNumber)
+
+	if(!isNaN(userNumber) && userNumber % 1 == 0) {
+		fizzBuzz(userNumber);
+		
+	} else {
+		alert('not a number')
 	}
-	else if (i%5 == 0) {
-		console.log ("buzz");
-	}
-	else {
-		console.log (i);
-	}
-}
+
+
+$('#textOutput').append(text);
 });
